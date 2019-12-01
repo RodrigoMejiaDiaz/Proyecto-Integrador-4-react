b561eee40a5f0a2b349edd24f3fcf54609e47362
Rodrigo Mejia Diaz <rodrigoemdiaz@hotmail.com>
2019-11-30 20:34:46 -0500

    Tabla Productos
    Se ha hecho una prueba de corsheaders para ver la tabla de productos

--- integrador4/src/App.js
+++ integrador4/src/App.js
@@ -1,26 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productos: [],
+      recuperado: false,
+    };
+  }
+
+  mostrarTabla() {
+    return (
+      <div>
+        <table border="1">
+          <thead>
+            <tr>
+              <th>Codigo</th>
+              <th>Producto</th>
+              <th>Descripcion</th>
+              <th>Codigo Categoria</th>
+              <th>Precio</th>
+              <th>Stock</th>
+              <th>Codigo Proveedor</th>
+              <th>Image</th>
+              <th>Estado</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.productos.map(producto => {
+              return (
+                <tr key={producto.cod_prod}>
+                  <td>{producto.cod_prod}</td>
+                  <td>{producto.producto}</td>
+                  <td>{producto.descripcion}</td>
+                  <td>{producto.cod_cat}</td>
+                  <td>{producto.precio}</td>
+                  <td>{producto.stock}</td>
+                  <td>{producto.cod_prov}</td>
+                  <td>{producto.image}</td>
+                  <td>{producto.estado}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    fetch('http://192.168.244.129:8000/api/productos')
+    .then((response) => {
+      return response.json()
+    })
+    .then((prod) => {
+      this.setState({
+        productos: prod,
+        recuperado: true,
+      })
+    })
+  }
+
+  render() {
+    if (this.state.recuperado)
+      return this.mostrarTabla()
+    else
+      return (<div>recuperando datos...</div>)
+  }
 }
 
 export default App;
--- integrador4/src/index.js
+++ integrador4/src/index.js
@@ -1,12 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Producto extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        productos: [],
+        recuperado: false,
+      };
+    }
+  
+    mostrarTabla() {
+      return (
+        <div>
+          <table border="1">
+            <thead>
+              <tr>
+                <th>Codigo</th>
+                <th>Producto</th>
+                <th>Descripcion</th>
+                <th>Codigo Categoria</th>
+                <th>Precio</th>
+                <th>Stock</th>
+                <th>Codigo Proveedor</th>
+                <th>Image</th>
+                <th>Estado</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.productos.map(producto => {
+                return (
+                  <tr key={producto.cod_prod}>
+                    <td>{producto.cod_prod}</td>
+                    <td>{producto.producto}</td>
+                    <td>{producto.descripcion}</td>
+                    <td>{producto.cod_cat}</td>
+                    <td>{producto.precio}</td>
+                    <td>{producto.stock}</td>
+                    <td>{producto.cod_prov}</td>
+                    <td>{producto.image}</td>
+                    <td>{producto.estado}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+  
+    componentDidMount() {
+      fetch('http://192.168.244.129:8000/api/productos/')
+      .then((response) => {
+        return response.json()
+      })
+      .then((prod) => {
+        this.setState({
+          productos: prod,
+          recuperado: true,
+        })
+      })
+    }
+  
+    render() {
+      if (this.state.recuperado)
+        return this.mostrarTabla()
+      else
+        return (<div>recuperando datos...</div>)
+    }
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(
+    <Producto />,
+    document.getElementById('root')
+);
+
 serviceWorker.unregister();

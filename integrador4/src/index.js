dfe754833fcc30cee6d1296d615a989134002ef0
Rodrigo Mejia Diaz <rodrigoemdiaz@hotmail.com>
2019-11-30 20:39:48 -0500

    Se ha añadido Bootstrap

--- integrador4/src/index.js
+++ integrador4/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class Producto extends React.Component {
     constructor(props) {
